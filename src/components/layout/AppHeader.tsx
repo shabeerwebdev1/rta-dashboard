6622bc7054d52e65c7b0c32f76a4a92c824ba6c3
Shabeer Ahamed <shabeerahamed.samsudeen@staples.com>
2025-07-29 18:16:49 +0530

    NODE_VERSION

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -23,7 +23,7 @@ const AppHeader = () => {
     <Header
       style={{
         padding: "0 24px",
-        background: "var(--ant-color-bg-container)",
+        // background: "var(--ant-color-bg-container)",
         position: "sticky",
         top: 0,
         zIndex: 999,
