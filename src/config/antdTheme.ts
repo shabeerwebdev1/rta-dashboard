42da8e5fd68d4b591bed8cb7ca8297123e543d5d
Shabeer Ahamed <shabeerahamed.samsudeen@staples.com>
2025-07-30 11:14:34 +0530

    update light theme

--- src/config/antdTheme.ts
+++ src/config/antdTheme.ts
@@ -1,52 +1,52 @@
 import type { ThemeConfig } from "antd";
 
-const baseConfig: ThemeConfig = {
-  token: {
-    fontFamily: "NoirPro, -apple-system, sans-serif",
-    borderRadius: 8,
-  },
-};
-
-const paletteCorporateLight = {
-  brightRed: "#EB2D2F",
-  darkRed: "#B92324",
-  vibrantGreen: "#017B4A",
-  darkGreen: "#01613B",
-  goldYellow: "#FCB500",
-  royalBlue: "#093D9E",
-  darkRoyalBlue: "#073180",
-  offWhiteCream: "#EDE7D1",
-  stoneBeige: "#C4BCA9",
-  black: "#000000",
-  white: "#FFFFFF",
-  white2: "#eef0f2",
+const palette = {
+  deepIndigo: "#171B7D",
+  steelBlue: "#5A7D9A",
+  brightRed: "#EB2630",
+  offWhite: "#F7F7F7",
+  lightGray: "#E3E3E3",
+  slateGray: "#444C55",
+  offBlack: "#232020",
+  mutedText: "#8B7F7F",
 };
 
 export const corporateLightThemeConfig: ThemeConfig = {
-  ...baseConfig,
   token: {
-    ...baseConfig.token,
-    colorPrimary: paletteCorporateLight.vibrantGreen,
-    colorError: paletteCorporateLight.brightRed,
-    colorWarning: paletteCorporateLight.goldYellow,
-    colorInfo: paletteCorporateLight.royalBlue,
-    colorBgLayout: paletteCorporateLight.white,
-    colorBgContainer: paletteCorporateLight.white,
+    fontFamily: "NoirPro, -apple-system, sans-serif",
+    borderRadius: 8,
+    colorPrimary: palette.deepIndigo,
+    colorError: palette.brightRed,
+    colorInfo: palette.steelBlue,
+    colorText: palette.offBlack,
+    colorTextSecondary: palette.brightRed,
+    colorBgLayout: palette.lightGray,
+    colorBgContainer: palette.offWhite,
+    colorBorder: palette.lightGray,
+    colorBorderSecondary: palette.lightGray,
   },
   components: {
     Layout: {
-      headerBg: paletteCorporateLight.white,
-      siderBg: paletteCorporateLight.darkGreen,
+      headerBg: palette.offWhite,
+      siderBg: palette.offBlack,
     },
     Menu: {
       colorItemBg: "transparent",
-      colorItemText: paletteCorporateLight.stoneBeige,
-      colorItemTextHover: paletteCorporateLight.white,
-      colorItemTextSelected: paletteCorporateLight.goldYellow,
-      colorItemBgSelected: "rgba(252, 181, 0, 0.15)",
+      colorItemText: palette.lightGray,
+      colorItemTextHover: palette.offWhite,
+      colorItemTextSelected: palette.offWhite,
+      colorItemBgSelected: palette.deepIndigo,
     },
     Button: {
       primaryShadow: "none",
+    },
+    Modal: {
+      contentBg: palette.offWhite,
+      headerBg: palette.offWhite,
+    },
+    Table: {
+      headerBg: palette.offWhite,
+      colorBgContainer: palette.offWhite,
     },
   },
 };
